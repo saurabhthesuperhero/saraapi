0bd35193147f51cc3f58dc33d2c3e1119c0d1f76
saurabhthesuperhero <saurabhthesuperhero@gmail.com>
2021-06-18 21:43:55 +0530

    dl added

--- app.py
+++ app.py
@@ -11,19 +11,44 @@ import wikipedia
 # Load data preprocessing libs
 import pandas as pd
 import numpy as np
+import random
+import json
 
+import torch
+
+from model import NeuralNet
+from nltk_utils import bag_of_words, tokenize
 # Load vectorizer and similarity measure
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-#Initializations
+# Initializations
 
-app=Flask(__name__)
+app = Flask(__name__)
 WIT_API_HOST = os.getenv('WIT_URL', 'https://api.wit.ai')
 WIT_API_VERSION = os.getenv('WIT_API_VERSION', '20200513')
 access_token = "ADRPSCFWVJEDDBCIYEP4ZFLY3EREFPYC"
 client = Wit(access_token)
 
-#Flask run
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+with open('intents.json', 'r') as json_data:
+    intents = json.load(json_data)
+
+FILE = "data.pth"
+data = torch.load(FILE)
+
+input_size = data["input_size"]
+hidden_size = data["hidden_size"]
+output_size = data["output_size"]
+all_words = data['all_words']
+tags = data['tags']
+model_state = data["model_state"]
+
+model = NeuralNet(input_size, hidden_size, output_size).to(device)
+model.load_state_dict(model_state)
+model.eval()
+# Flask run
 if __name__ == '__main__':
     app.run()
 
@@ -35,11 +60,12 @@ shy = ['you make me shy', 'cause my creator is awesome']
 love = ['I love eating lot', 'hehe , nice to hear']
 creator = ['Saurabh is my creator', 'Saurabh is AI developer.']
 
-#Homepage
+# Homepage
+
+
 @app.route('/')
 def Index():
     return "Hello I am Sara!"
-
 
 
 def check_intent(obj, key, name):
@@ -80,87 +106,93 @@ def translate(text, language):
         return "I didnt get destination language."
 
 
-def searchf(subj):
+def searchf(params,subj):
     try:
         result = wikipedia.summary(subj, sentences=2)
         return unidecode(result)
     except:
-        return "Sorry ..Didnt get it...learning"
+        return deepChat(params)
 
 
-def handle_message(response):
+def handle_message(params):
     try:
+        response=client.message(params)
         print(json.dumps(json.loads(json.dumps(response)), indent=2))
-        greeting = first_value(response['traits'], 'wit$greetings')
-        thamk = first_value(response['traits'], 'wit$thanks')
         typeof = first_value(response['entities'], 'type:type')
-        bye = first_value(response['traits'], 'wit$bye')
-        create = first_value(response['traits'], 'shy')
-        shy_ = first_value(response['traits'], 'creator')
-        love_ = first_value(response['traits'], 'love')
         checkcap = check_intent(response['intents'], 'name', 'capacity')
-        checktranslate = check_intent(response['intents'], 'name', 'phrase_translate')
+        checktranslate = check_intent(
+            response['intents'], 'name', 'phrase_translate')
         checksearch = check_intent(response['intents'], 'name', 'question')
-        # print(checktranslate)
-        tbody = first_value(response['entities'], 'wit$phrase_to_translate:phrase_to_translate')
-        tlanguage = first_value(response['entities'], 'wit$message_subject:message_subject')
-        ssubject = first_value(response['entities'], 'wit$message_subject:message_subject')
-        snotable = first_value(response['entities'], 'wit$wikipedia_search_query:wikipedia_search_query')
-        swiki = first_entityvalue(response['entities'], 'wit$notable_person:notable_person')
+        tbody = first_value(
+            response['entities'], 'wit$phrase_to_translate:phrase_to_translate')
+        tlanguage = first_value(
+            response['entities'], 'wit$message_subject:message_subject')
+        ssubject = first_value(
+            response['entities'], 'wit$message_subject:message_subject')
+        snotable = first_value(
+            response['entities'], 'wit$wikipedia_search_query:wikipedia_search_query')
+        swiki = first_entityvalue(
+            response['entities'], 'wit$notable_person:notable_person')
 
         if checkcap:
             return "Hello,I can chat with you, & translate to any language, like:translate hello in tamil, I can also give you information on topics."
         elif checksearch:
             if (snotable != None):
-                return searchf(snotable)
+                return searchf(params,snotable)
             elif (ssubject != None):
-                return searchf(ssubject)
+                return searchf(params,ssubject)
             elif (swiki != None):
-                return searchf(swiki)
+                return searchf(params,swiki)
             else:
                 return "Didnt got it,will learn and tell you."
         elif checktranslate:
             return translate(tbody, tlanguage)
-        elif typeof != None:
-            if typeof.lower() == "movie":
-                return "Movie you can watch"
-        elif bye:
-            return "Bye see you later"
-        elif greeting:
-            return random.choice(greet)
-        elif thamk:
-            return random.choice(thankk)
-        elif create:
-            return random.choice(creator)
-        elif shy_:
-            return random.choice(shy)
-        elif love_:
-            return random.choice(love)
         else:
-            return "........oooooo"
+            return deepChat(params)
     except Exception as e:
         return "Soryy Im learning dear, will be awesome soon"
 
+def deepChat(sentence):
+    sentence = tokenize(sentence)
+    X = bag_of_words(sentence, all_words)
+    X = X.reshape(1, X.shape[0])
+    X = torch.from_numpy(X).to(device)
 
+    output = model(X)
+    _, predicted = torch.max(output, dim=1)
 
+    tag = tags[predicted.item()]
 
+    probs = torch.softmax(output, dim=1)
+    data=[]
+    prob = probs[0][predicted.item()]
+    if prob.item() > 0.75:
+        for intent in intents['intents']:
+            if tag == intent["tag"]:
+                message= random.choice(intent['responses'])
+                return message
 
+    else:
+        message="Soryy Im learning dear, will be awesome soon"
+        return message
 
 @app.route('/call=<lstring>')
 def mainapp(lstring):
     data = []
     params = lstring
-    data.append({"message": handle_message(client.message(params))})
+    data.append({"message": handle_message(params)})
+    # return deepChat(lstring)
     return jsonify(data=data, status=200)
+
 
 @app.route('/call2=<lstring>')
 def mainapp2(lstring):
-    data=[]
+    data = []
     df = pd.read_csv("DFs1s2.csv")
     df.dropna(inplace=True)
     vectorizer = TfidfVectorizer()
     vectorizer.fit(np.concatenate((df.p1, df.p2)))
-    Question_vectors = vectorizer.transform(df.p1)    
+    Question_vectors = vectorizer.transform(df.p1)
     # Locate the closest question
     input_question_vector = vectorizer.transform([lstring])
 
@@ -171,6 +203,6 @@ def mainapp2(lstring):
     closest = np.argmax(similarities, axis=1)
 
     # Print the correct answer
-    message=(df.p2.iloc[closest].values[0])
+    message = (df.p2.iloc[closest].values[0])
     data.append({"message": message})
-    return jsonify(data=data, status=200)    
+    return jsonify(data=data, status=200)
